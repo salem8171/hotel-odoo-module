a8c14b8c18133b82635b538ab76775e837b206df
YOSR BEN HAMIDA <yosr.benhamida@enis.tn>
2019-12-15 15:53:43 +0100

    'first'

--- __manifest__.py
+++ __manifest__.py
@@ -25,7 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'views/EtageChambre_views.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
